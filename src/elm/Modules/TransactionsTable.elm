7ca1e9517becfc64027c8033d2710e1ca95b787b
sebpearce <sebpearce@gmail.com>
2016-11-27 18:29:29 +1100

    Add delete functionality

--- src/elm/Modules/TransactionsTable.elm
+++ src/elm/Modules/TransactionsTable.elm
@@ -6,6 +6,8 @@ import Html.Attributes exposing (..)
 import Dict exposing (..)
 import Modules.Helpers exposing (renderDate, renderAmount, formatAsMoney, applyColor)
 import Modules.Transaction exposing (..)
+import Html.Events exposing (onClick)
+import Msg exposing (..)
 
 
 categoryName : Maybe String -> String
@@ -18,29 +20,33 @@ categoryName category =
             ""
 
 
-renderTransactionRow : Model -> Transaction -> Html msg
+renderTransactionRow : Model -> Transaction -> Html Msg
 renderTransactionRow model transaction =
-    div [ class "transactions-table__day__row" ]
+    div [ class "transactions-table__day__row", attribute "data-id" (toString transaction.id) ]
         [ div [ class "transactions-table__day__row__cat" ] [ text (categoryName (Dict.get transaction.category model.categories)) ]
         , div [ class ("transactions-table__day__row__amt" ++ applyColor transaction.amount) ] [ renderAmount transaction.amount ]
         , div [ class "transactions-table__day__row__desc" ] [ text transaction.description ]
+        , a [ class "transactions-table__day__row__delete-link", onClick (DeleteTransaction transaction.id) ] [ text "x" ]
         ]
 
 
-renderTransactionsForOneDay : Model -> TransactionsForOneDay -> Html msg
+renderTransactionsForOneDay : Model -> TransactionsForOneDay -> Html Msg
 renderTransactionsForOneDay model ( date, transactions ) =
     let
         oneDayOfTransactions =
             List.map (renderTransactionRow model) transactions
     in
-        div [ class "transactions-table__day" ]
-            [ div [ class "transactions-table__day__date" ] [ text (renderDate date) ]
-            , div [ class "transactions-table__day__transactions" ]
-                oneDayOfTransactions
-            ]
+        if List.isEmpty transactions == True then
+            text ""
+        else
+            div [ class "transactions-table__day" ]
+                [ div [ class "transactions-table__day__date" ] [ text (renderDate date) ]
+                , div [ class "transactions-table__day__transactions" ]
+                    oneDayOfTransactions
+                ]
 
 
-renderTransactionsTable : Model -> Html msg
+renderTransactionsTable : Model -> Html Msg
 renderTransactionsTable model =
     let
         renderedDays =
